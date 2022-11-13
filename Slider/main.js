912b41226add99c6e255c46d0358e5a8fc6c4d2f
Jurand Plaskota <jurand.plaskota@gmail.com>
2022-11-13 02:40:50 +0100

    slider part 2 js works

--- Slider/main.js
+++ Slider/main.js
@@ -1,3 +1,41 @@
-const imageArr = ['./images/img1.jpg', './images/img2.jpg', './images/img3.jpg', './images/img4.jpg', './images/img5.jpg'];
+const imageArr = [
+  "./images/img1.jpg",
+  "./images/img2.jpg",
+  "./images/img3.jpg",
+  "./images/img4.jpg",
+  "./images/img5.jpg",
+];
 
-const slider = document.querySelector('.slider');
+const btn = Array.from(document.getElementsByClassName("btn"));
+const slider = document.getElementById("slider");
+const img = document.createElement("img");
+
+let counter = 0;
+
+document.onload = Render(counter);
+
+btn.map((el) => {
+  el.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "prev":
+        if (counter > 0) {
+          counter--;
+        }
+        Render(counter);
+        break;
+
+      case "next":
+        if (counter < imageArr.length - 1) {
+          counter++;
+        }
+        Render(counter);
+        break;
+    }
+  });
+});
+
+function Render(counter) {
+  console.log("id " + counter);
+  img.src = imageArr[counter];
+  slider.appendChild(img);
+}
