999b14de4a91fab2bf343062671c4d592ee9d687
Jurand Plaskota <jurand.plaskota@gmail.com>
2022-11-30 02:15:14 +0100

    localsotrage done render all done pin done color bar in progress ...

--- NotePocket/notes.js
+++ NotePocket/notes.js
@@ -1,20 +1,42 @@
-let title = document.querySelector("[data-title]");
-let content = document.querySelector("[data-content]");
-let notesContainer = document.getElementById("main");
+const title = document.querySelector("[data-title]");
+const content = document.querySelector("[data-content]");
+const notesContainer = document.getElementById("main");
 
 const newBtn = Array.from(document.getElementsByClassName("new-btn"));
 
-const notes = JSON.parse(localStorage.getItem("notes")) || [];
+let notes = [];
+
+document.getElementById("clear-all").addEventListener("click", (e) => {
+  localStorage.clear();
+  notesContainer.innerHTML = "";
+  console.log("notes cleared");
+});
+
+window.addEventListener("load", (e) => {
+  console.log("page is fully loaded");
+  notes = JSON.parse(localStorage.getItem("notes")) || [];
+  console.log("notes lenght: " + notes.length);
+  if (notes.length > 0) {
+    for (let i = 0; i < notes.length; i++) {
+      renderNotes(notes[i]);
+    }
+  }
+});
 
 newBtn.map((el) => {
   el.addEventListener("click", (e) => {
     switch (e.target.innerText) {
       case "Save":
-        newNote(title.textContent, content.textContent);
+        if (newNote(title.textContent, content.textContent) !== false) {
+          title.textContent = "";
+          content.textContent = "";
+          title.focus();
+        }
         break;
       case "Close":
         title.textContent = "";
         content.textContent = "";
+        title.focus();
         localStorage.clear();
         break;
     }
@@ -26,36 +48,41 @@ function newNote(xTitle, xContent) {
   if (xTitle === "") {
     //TODO add alert
     console.log("empty title");
-    return;
+    return false;
   }
   const note = {
     title: xTitle,
     content: xContent,
     date: new Date().toLocaleDateString(),
     time: new Date().toLocaleTimeString(),
+    pinned: false,
   };
+
+  //save to local storage
   notes.push(note);
+  localStorage.setItem("notes", JSON.stringify(notes));
+
   renderNotes(note);
 }
 
-// function saveNotes(note) {
-//   const notes = JSON.parse(localStorage.getItem("notes")) || [];
-//   notes.push(note);
-//   localStorage.setItem("notes", JSON.stringify(notes));
-//   renderNotes(note);
-//   for (let i = 0; i < notes.length; i++) {
-//     console.log(notes[i]);
-//   }
-// }
-
-//TODO render more than one note
 function renderNotes(xNote) {
-  console.log(xNote);
-
   //note
   const note = document.createElement("div");
-  note.className = "note";
+  if (xNote.pinned === true) {
+    note.className = "note pinned";
+  } else {
+    note.className = "note";
+  }
   notesContainer.appendChild(note);
+
+  //note pin
+  const notePin = document.createElement("div");
+  if (xNote.pinned === true) {
+    notePin.className = "note-pin pin-visible";
+  } else {
+    notePin.className = "note-pin";
+  }
+  note.appendChild(notePin);
 
   //note title and content
   const noteTitle = document.createElement("div");
@@ -86,6 +113,20 @@ function renderNotes(xNote) {
   colorBtn.appendChild(colorIcon);
 
   const pinBtn = document.createElement("button");
+  pinBtn.addEventListener("click", (e) => {
+    if (xNote.pinned === false) {
+      note.className = "note pinned";
+      notePin.className = "note-pin pin-visible";
+      xNote.pinned = true;
+    } else {
+      note.className = "note";
+      notePin.className = "note-pin";
+      xNote.pinned = false;
+    }
+    notes.splice(notes.indexOf(xNote), 1);
+    notes.push(xNote);
+    localStorage.setItem("notes", JSON.stringify(notes));
+  });
   pinBtn.id = "pin";
   menu.appendChild(pinBtn);
 
@@ -95,6 +136,11 @@ function renderNotes(xNote) {
 
   // note right icon
   const deleteBtn = document.createElement("button");
+  deleteBtn.addEventListener("click", (e) => {
+    note.remove();
+    notes.splice(notes.indexOf(xNote), 1);
+    localStorage.setItem("notes", JSON.stringify(notes));
+  });
   deleteBtn.id = "delete";
   nav.appendChild(deleteBtn);
 
