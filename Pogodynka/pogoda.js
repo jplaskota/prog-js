15030e5e804a01643fa63d8c34fe14f509bafbe0
jurandplaskota <jurand.plaskota@microsoft.wsei.edu.pl>
2023-01-14 02:41:06 +0100

    working api, save, delete all, render

--- Pogodynka/pogoda.js
+++ Pogodynka/pogoda.js
@@ -1,25 +1,135 @@
+//in
 const searchCity = document.querySelector("[data-search]");
+const add = document.querySelector("[data-add]");
+const format = document.querySelector("[data-format]");
+const saved = document.querySelector("[data-saved]");
+
+//out
 const city = document.querySelector("[data-city]");
+const temp = document.querySelector("[data-temp]");
+const desc = document.querySelector("[data-desc]");
+const tempMin = document.querySelector("[data-temp-min]");
+const tempMax = document.querySelector("[data-temp-max]");
+
+let savedCity = [];
 
 window.addEventListener("load", (e) => {
   console.log("page is fully loaded");
+  loadSaved();
 });
 
-window.addEventListener("keypress", (e) => {
-  if (e.code === "Enter") {
-    console.log("Enter pressed");
-    searchCity.blur();
-    city.textContent = searchCity.textContent;
+async function loadSaved() {
+  savedCity = JSON.parse(localStorage.getItem("city")) || [];
+  console.log("Loaded city: " + savedCity.length);
 
-    const data = fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=Kraków&appid=9872a8ba6858e932cea3bacb69631da1",
+  saved.innerHTML = "";
+
+  if (savedCity.length > 0) {
+    for (let i = 0; i < savedCity.length; i++) {
+      renderCity(savedCity[i]);
+    }
+  }
+}
+
+window.addEventListener("keypress", async (e) => {
+  if (e.code === "Enter") {
+    searchCity.blur();
+    console.log("Saved: " + savedCity.length);
+
+    if (searchCity.textContent === "") {
+      console.log("No city name");
+      return;
+    }
+
+    const { main, name, weather } = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        searchCity.textContent +
+        "&appid=9872a8ba6858e932cea3bacb69631da1",
       { method: "GET" }
     )
-      .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((res) => {
+        if (res.ok) {
+          searchCity.textContent = null;
+          return res.json();
+        } else {
+          console.log("City not found");
+          return;
+        }
+      })
+      .catch((err) => console.log(err));
+
+    console.log("Searched city: " + name);
+
+    city.textContent = name;
+    temp.textContent = (main.temp - 273.15).toFixed(0) + "°";
+    desc.textContent = weather[0].description;
+    tempMin.textContent = "↓ " + (main.temp_min - 273.15).toFixed(0) + "°";
+    tempMax.textContent = "↑ " + (main.temp_max - 273.15).toFixed(0) + "°";
   }
 });
 
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+add.addEventListener("click", (e) => {
+  if (city.textContent === "") {
+    console.log("No city name");
+    return;
+  }
 
+  if (savedCity.length > 0) {
+    for (let i = 0; i < savedCity.length; i++) {
+      if (savedCity[i].name === city.textContent) {
+        console.log("City already saved");
+        return;
+      }
+    }
+  }
+
+  savedCity.push({
+    name: city.textContent,
+    temp: temp.textContent,
+    desc: desc.textContent,
+    tempMin: tempMin.textContent,
+    tempMax: tempMax.textContent,
+  });
+
+  localStorage.setItem("city", JSON.stringify(savedCity));
+  loadSaved();
+
+  console.log("City saved: " + city.textContent);
+});
+
+format.addEventListener("click", (e) => {
+  savedCity.length = 0;
+  localStorage.clear();
+  loadSaved();
+  saved.innerHTML = "";
+  console.log("Local storage cleared. Length: " + savedCity.length);
+});
+
+async function renderCity(city) {
+  const cityContainer = document.createElement("div");
+  cityContainer.classList.add("saved", "bgc");
+  saved.appendChild(cityContainer);
+
+  const box = document.createElement("div");
+  box.classList.add("savedBox");
+  cityContainer.appendChild(box);
+
+  const cityName = document.createElement("div");
+  cityName.classList.add("city");
+  cityName.textContent = city.name;
+  box.appendChild(cityName);
+
+  const cityTemp = document.createElement("div");
+  cityTemp.classList.add("temp");
+  cityTemp.textContent = city.temp;
+  box.appendChild(cityTemp);
+
+  const img = document.createElement("img");
+  img.src = "https://picsum.photos/id/30/30";
+  cityContainer.appendChild(img);
+}
+
+// TODO: function to get weather data from API (obj problem ...)
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 // 9872a8ba6858e932cea3bacb69631da1
