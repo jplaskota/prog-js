1dfbe9c36d1f25973e5ffc7bdfdbf7452e3ca654
Jurand Plaskota <jurand.plaskota@gmail.com>
2024-03-02 03:38:39 +0100

    app layout pro > free (openweathermap api)

--- Pogodynka/pogoda.js
+++ Pogodynka/pogoda.js
@@ -128,9 +128,9 @@ async function remove(cityName) {
 // download weather data by city name
 async function getWeather(cityName) {
   const { city, list } = await fetch(
-    "https://pro.openweathermap.org/data/2.5/forecast/climate?q=" +
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
       cityName +
-      "&cnt=7&mode=json&units=metric&appid=e56ebc4991608107818d622503afefbe",
+      "&cnt=6&mode=json&units=metric&appid=e56ebc4991608107818d622503afefbe",
     { method: "GET" }
   )
     .then((res) => {
@@ -149,96 +149,39 @@ async function getWeather(cityName) {
   };
 }
 
-// download hours data by city name
-async function getHours(cityName) {
-  const { list } = await fetch(
-    "https://pro.openweathermap.org/data/2.5/forecast/hourly?q=" +
-      cityName +
-      "&cnt=6&mode=json&units=metric&appid=e56ebc4991608107818d622503afefbe",
-    { method: "GET" }
-  )
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      } else {
-        error("Error with hours fetch");
-        throw new Error("Error with hours fetch");
-      }
-    })
-    .catch((err) => console.log(err));
-
-  return {
-    hours: list,
-  };
-}
-
 // change the current weather
 async function changeCurrent(cityName) {
   const { name, list } = await getWeather(cityName);
 
   city.textContent = name;
-  temp.textContent = list[0].temp.day.toFixed(0) + "°";
+  temp.textContent = list[0].main.temp.toFixed(0) + "°";
   desc.textContent = list[0].weather[0].description;
-  tempMin.textContent = "↓ " + list[0].temp.min.toFixed(0) + "°";
-  tempMax.textContent = "↑ " + list[0].temp.max.toFixed(0) + "°";
+  tempMin.textContent = "↓ " + list[0].main.temp_min.toFixed(0) + "°";
+  tempMax.textContent = "↑ " + list[0].main.temp_max.toFixed(0) + "°";
 
-  daysBox.innerHTML = "";
   hoursBox.innerHTML = "";
-
-  const { hours } = await getHours(cityName);
 
   function pad(d) {
     return d < 10 ? "0" + d.toString() : d.toString();
   }
 
-  // add 5 next hours
+  // add 5 more hours forecast (each +3h)
   for (let i = 1; i < 6; i++) {
     const box = document.createElement("div");
     box.classList.add("hour");
     hoursBox.appendChild(box);
 
     const hour = document.createElement("p");
-    hour.textContent = pad((new Date().getHours() + i) % 24);
+    hour.textContent = pad((new Date().getHours() + i * 3) % 24);
     box.appendChild(hour);
 
     const img = document.createElement("img");
-    img.src = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${hours[i].weather[0].icon}.svg`;
+    img.src = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${list[i].weather[0].icon}.svg`;
     box.appendChild(img);
 
     const temp = document.createElement("p");
-    temp.textContent = hours[i].main.temp.toFixed(0) + "°";
+    temp.textContent = list[i].main.temp.toFixed(0) + "°";
     box.appendChild(temp);
-  }
-
-  // add 5 next days of weekend
-  for (let i = 1; i < 6; i++) {
-    const box = document.createElement("div");
-    box.classList.add("day", "bgc");
-    daysBox.appendChild(box);
-
-    const dataBox = document.createElement("div");
-    dataBox.classList.add("dayBox");
-    box.appendChild(dataBox);
-
-    const daysWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-    const dayName = document.createElement("div");
-    dayName.classList.add("dayName");
-    dayName.textContent = daysWeek[(new Date().getDay() + i) % 7];
-    dataBox.appendChild(dayName);
-
-    const temp = document.createElement("div");
-    temp.classList.add("dayTemp");
-    temp.textContent = list[i].temp.day.toFixed(0) + "°";
-    dataBox.appendChild(temp);
-
-    const imgBox = document.createElement("div");
-    imgBox.classList.add("imgBox");
-    box.appendChild(imgBox);
-
-    const img = document.createElement("img");
-    img.src = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${list[i].weather[0].icon}.svg`;
-    imgBox.appendChild(img);
   }
 }
 
@@ -282,7 +225,7 @@ async function renderCity(cityName) {
 
   const cityTemp = document.createElement("div");
   cityTemp.classList.add("temp");
-  cityTemp.textContent = list[0].temp.day.toFixed(0) + "°";
+  cityTemp.textContent = list[0].main.temp.toFixed(0) + "°";
   box.appendChild(cityTemp);
 
   const cityImgBox = document.createElement("div");
@@ -293,11 +236,5 @@ async function renderCity(cityName) {
   img.src = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${list[0].weather[0].icon}.svg`;
   cityImgBox.appendChild(img);
 }
-
-// [max 30 days weather forecast]
-// https://pro.openweathermap.org/data/2.5/forecast/climate?q={city name}&cnt={number of days}&mode=json&units=metric&appid={API key}
-
-// [hourly forecast]
-// https://pro.openweathermap.org/data/2.5/forecast/hourly?q={city name}&cnt={number of hours}&mode=json&units=metric&appid={API key}
 
 // e56ebc4991608107818d622503afefbe
